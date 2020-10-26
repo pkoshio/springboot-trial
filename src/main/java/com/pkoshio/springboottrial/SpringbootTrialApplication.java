3b2fc7974daaf7d224d098f64b202c1d4a5de54c
pkoshio <pkoshio0788@gmail.com>
2020-10-27 00:47:19 +0900

    first commit

---
+++ src/main/java/com/pkoshio/springboottrial/SpringbootTrialApplication.java
@@ -0,0 +1,13 @@
+package com.pkoshio.springboottrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkoshio/springboottrial/SpringbootTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkoshio.springboottrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
